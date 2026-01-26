e60b45317ce11678b70ebc31396e319db50a17d9
yohan <yohan.penaloza@gmail.com>
2026-01-25 22:26:30 -0500

    Cambio de comando instalación

--- pytest/run_test.sh
+++ pytest/run_test.sh
@@ -3,7 +3,7 @@ echo "Activando entorno virtual"
 source venv/bin/activate
 echo "Instalando dependencias"
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install pytest pytest-html
 echo "Ejecutando pruebas"
 pytest test/ --junitxml=reports/test-result.xml --html=reports/test-result.html --self-contained-html
 echo "Pruebas ejecutadas"
